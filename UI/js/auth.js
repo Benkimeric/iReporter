86255132dca077afc77a32052cb241c4c9828c08
Benkim <benkimeric@gmail.com>
2018-12-29 17:02:54 +0300

    [Feature #162869177] consume signup endpoint

--- UI/js/auth.js
+++ UI/js/auth.js
@@ -42,3 +42,56 @@ function Login(e) {
     })
     .catch((err) => console.log(err))
 }
+
+// sign up
+var signup_form = document.getElementById('signup');
+  if(signup_form){
+    signup_form.addEventListener('submit', Signup);
+  }
+
+function Signup(e) {
+  e.preventDefault();
+
+  let fname = document.getElementById('fname').value;
+  let lname = document.getElementById('lname').value;
+  let mname = document.getElementById('mname').value;
+  let username = document.getElementById('username').value;
+  let email = document.getElementById('email').value;
+  let phone = document.getElementById('phone').value;
+  let pass = document.getElementById('pass').value;
+  let pass2 = document.getElementById('pass2').value;
+
+  fetch(`${base_URL}auth/signup`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      first_name: fname,
+      last_name: lname,
+      other_names: mname,
+      email: email,
+      phone_number: phone,
+      user_name: username,
+      password: pass,
+      confirm_password: pass2
+    })
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.status == 201) {
+        // console.log(data)
+        alert("Account created successfully. Click OK to login");
+        window.location.href = "login.html";
+      }
+      else {
+        let signup_error_label = document.getElementById('signup_error_label')
+        if (signup_error_label) {
+          signup_error_label.innerHTML = data.message
+        }
+      }
+    })
+    .catch((err) => console.log(err))
+}
+
